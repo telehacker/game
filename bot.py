3575e3162ef20bf124fdf5a6385a85ddc835044c
telehacker <135429665+telehacker@users.noreply.github.com>
2026-01-05 21:55:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,7 +37,7 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ForceReply
 # ---------------------------
 # CONFIG
 # ---------------------------
-TOKEN = os.environ.get("TELEGRAM_TOKEN", "")
+TOKEN = os.environ.get("TELEGRAM_TOKEN", "8208557623:AAHMVlODlQMpaoQ-PRFzVXOue-ousiWhu_Y")
 if not TOKEN:
     print("❌ TELEGRAM_TOKEN not set")
     sys.exit(1)
