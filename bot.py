8acd036f04f3d5c22449aa365339257aaadf68b3
telehacker <135429665+telehacker@users.noreply.github.com>
2026-01-05 22:52:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -50,10 +50,10 @@ SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP_LINK", "https://t.me/Ruhvaan")
 START_IMG_URL = "https://image2url.com/r2/default/images/1767379923930-426fd806-ba8a-41fd-b181-56fa31150621.jpg"
 
 # GitHub / OpenAI config (optional)
-GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
-REPO_OWNER = os.environ.get("REPO_OWNER") or os.environ.get("GITHUB_REPO_OWNER")
-REPO_NAME = os.environ.get("REPO_NAME") or os.environ.get("GITHUB_REPO_NAME")
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+GITHUB_TOKEN = os.environ.get("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")
+REPO_OWNER = os.environ.get("telehacker") or os.environ.get("telehacker")
+REPO_NAME = os.environ.get("game") or os.environ.get("game")
+OPENAI_API_KEY = os.environ.get("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")
 
 bot = telebot.TeleBot(TOKEN, parse_mode="HTML")
 app = Flask(__name__)
